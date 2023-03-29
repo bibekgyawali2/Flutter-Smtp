502fff5cf3281868b037d99a97d43cfb5730c06c
bibekgyawali2 <gyawali.bibek02@gmail.com>
2023-03-29 08:53:57 +0545

    google sign in created

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,8 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
+import 'package:mailer/mailer.dart';
+import 'package:mailer/smtp_server.dart';
+
+import 'auth.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -9,8 +12,28 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Future sendEmail() async {
+    final user = await GoogleAuthApi.signIn();
+    if (user == null) return;
+    final email = user.email;
+    final auth = await user.authentication;
+    final token = auth.accessToken;
+
+    final smtpServer = gmailSaslXoauth2(email[0], token.toString());
+    final message = Message()
+      ..from = Address(email[0], 'Bibek')
+      ..subject = 'hello'
+      ..text = 'This is test email';
+    await send(message, smtpServer);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {},
+        child: const Text('Send Email'),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'home.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
